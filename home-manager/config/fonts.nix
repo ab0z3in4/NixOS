bd671e637937cea32256e0a8dd881b86f936f99e
ab0z3in4 <169134542+ab0z3in4@users.noreply.github.com>
2025-02-21 13:49:43 +0200

    Create fonts.nix

---
+++ home-manager/config/fonts.nix
@@ -0,0 +1,10 @@
+{
+  fonts.fontconfig = {
+    enable = true;
+    defaultFonts = {
+      sansSerif = [ "DejaVu Sans" "Noto Naskh Arabic" ];
+      serif = [ "DejaVu Serif" "Noto Naskh Arabic" ];
+      monospace = [ "DejaVu Sans Mono" "Noto Naskh Arabic" ];
+    };
+  };
+}
