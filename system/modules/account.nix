4ed02cd5395e5bd89a7fe567457968dbd2cca5ee
ab0z3in4 <169134542+ab0z3in4@users.noreply.github.com>
2025-02-26 07:51:43 +0200

    Update account.nix

--- system/modules/account.nix
+++ system/modules/account.nix
@@ -1,14 +1,14 @@
-{ pkgs-unstable, ... }:
+{ pkgs, ... }:
 
 {
   users.users.ab0z3in4 = {
     isNormalUser = true;
     initialPassword = "password";
-    extraGroups = [ "wheel" "networkmanager" "audio" "video" "libvirtd" "kvm" ];
+    extraGroups = [ "wheel" "networkmanager" "audio" "video" "libvirtd" ];
   };
   programs.bash = {
     completion.enable = true;
-    completion.package = pkgs-unstable.bash-completion;
+    completion.package = pkgs.bash-completion;
     interactiveShellInit = "eval \"$(starship init bash)\"";
     shellAliases = {
       update = "sudo nixos-rebuild switch --flake /etc/nixos#ab0z3in4-PC";
