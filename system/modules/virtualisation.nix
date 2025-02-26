5f29a7d198b3eb40f18b1775c62a38e0647860a3
ab0z3in4 <169134542+ab0z3in4@users.noreply.github.com>
2025-02-26 08:00:46 +0200

    Update virtualisation.nix

--- system/modules/virtualisation.nix
+++ system/modules/virtualisation.nix
@@ -12,7 +12,7 @@
         };
         ovmf = {
           enable = true;
-          packages = [ pkgs-unstable.OVMFFull ];
+          packages = [ pkgs-unstable.OVMFFull.fd ];
         };
       };
     };
