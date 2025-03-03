463b57d48aa4a6bc09fe6ea05c7046fa5a2624e5
ab0z3in4 <169134542+ab0z3in4@users.noreply.github.com>
2025-03-03 15:51:37 +0200

    Update fonts.nix

--- system/modules/fonts.nix
+++ system/modules/fonts.nix
@@ -3,16 +3,12 @@
 {
   fonts.packages = with pkgs-unstable; [
     noto-fonts
-    noto-fonts-cjk-sans
     noto-fonts-emoji
     nerd-fonts.fira-code
     nerd-fonts.jetbrains-mono
     cantarell-fonts
-    cascadia-code
     fira-code
     fira-code-symbols
-    liberation_ttf
-    libertinus
     fira-sans
     hack-font
     font-awesome
