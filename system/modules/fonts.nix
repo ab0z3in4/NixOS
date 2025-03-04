d8c3edbb63aa6f61de40111d481ad5c10df6a89b
ab0z3in4 <169134542+ab0z3in4@users.noreply.github.com>
2025-03-04 10:13:36 +0200

    Update fonts.nix

--- system/modules/fonts.nix
+++ system/modules/fonts.nix
@@ -1,19 +1,28 @@
 { pkgs-unstable, ... }:
 
 {
-  fonts.packages = with pkgs-unstable; [
-    noto-fonts
-    noto-fonts-emoji
-    nerd-fonts.fira-code
-    nerd-fonts.jetbrains-mono
-    cantarell-fonts
-    fira-code
-    fira-code-symbols
-    fira-sans
-    hack-font
-    font-awesome
-    fira-code
-    jetbrains-mono
-    open-sans
-  ];
+  fonts {
+    packages = with pkgs-unstable; [
+      noto-fonts
+      noto-fonts-emoji
+      nerd-fonts.fira-code
+      nerd-fonts.jetbrains-mono
+      cantarell-fonts
+      fira-code
+      fira-code-symbols
+      fira-sans
+      hack-font
+      font-awesome
+      fira-code
+      jetbrains-mono
+      open-sans
+      roboto
+      merriweather
+    ];
+    fontconfig.defaultFonts = {
+      sansSerif = [ "Roboto" "Noto Naskh Arabic" ];
+      serif = [ "Merriweather" "Noto Naskh Arabic" ];
+      monospace = [ "JetBrains Mono" "Noto Naskh Arabic" ];
+    };
+  }  
 }
