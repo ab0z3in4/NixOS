a3a0bb3a0e126ca631bd11fb633afcf15b973921
ab0z3in4 <social.media.abozeina@gmail.com>
2025-03-06 10:40:28 +0200

    edit boot

--- system/modules/boot.nix
+++ system/modules/boot.nix
@@ -1,8 +1,16 @@
-{ lib, ... }:
+{ lib, pkgs-unstable, ... }:
 
 {
   boot = {
-    plymouth.enable = true;
+    plymouth = {
+      enable = true;
+      theme = "glitch";
+      themePackages = with pkgs-unstable; [
+        (adi1090x-plymouth-themes.override {
+          selected_themes = [ "glitch" ];
+        })
+      ];
+    };
     consoleLogLevel = 0;
     initrd.verbose = false;
     kernelParams = [
@@ -16,27 +24,44 @@
       "nohibernate"
       "apparmor=1"
       "security=apparmor"
+      "slab_nomerge"
+      "page_alloc.shuffle=1"
+      "pti=on"
+      "init_on_alloc=1"
+      "init_on_free=1"
     ];
     tmp.cleanOnBoot = true;
-    supportedFilesystems = [ "ntfs" ];
-    /*lanzaboote = {
-      enable = true;
-      pkiBundle = "/var/lib/sbctl";
-    };*/
+    supportedFilesystems = [ "ntfs" "btrfs" ];
     loader = {
       efi.canTouchEfiVariables = true;
       systemd-boot.enable = true;
       # systemd-boot.enable = lib.mkForce false;
       timeout = 0;
     };
-    kernelModules = [ "tcp_bbr" "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" "kvm-intel" ];
+    /*lanzaboote = {
+      enable = true;
+      pkiBundle = "/var/lib/sbctl";
+    };*/
+    kernelModules = [
+      "tcp_bbr"
+      "nvidia"
+      "nvidia_modeset"
+      "nvidia_uvm"
+      "nvidia_drm"
+      "kvm-intel"
+      "overlay"
+      "tun"
+    ];
     kernel.sysctl = {
       "net.ipv4.tcp_congestion_control" = "bbr";
-      "net.core.default_qdisc" = "fq";
+      "net.core.default_qdisc" = "fq_codel";
       "net.core.wmem_max" = 1073741824;
       "net.core.rmem_max" = 1073741824;
       "net.ipv4.tcp_rmem" = "4096 87380 1073741824";
       "net.ipv4.tcp_wmem" = "4096 87380 1073741824";
+      "net.ipv4.tcp_fin_timeout" = 15;
+      "net.ipv4.tcp_tw_reuse" = 1;
+      "net.ipv4.tcp_syncookies" = 1;
     };
   };
 }
