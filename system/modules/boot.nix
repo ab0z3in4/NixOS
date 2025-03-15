424069c66002381dcfb938518b866f2eb6847ecc
ab0z3in4 <169134542+ab0z3in4@users.noreply.github.com>
2025-03-15 18:58:49 +0200

    Update boot.nix

--- system/modules/boot.nix
+++ system/modules/boot.nix
@@ -1,26 +1,8 @@
-{ lib, pkgs-unstable, ... }:
+{ lib, ... }:
 
 {
   boot = {
-    plymouth = {
-      enable = true;
-      theme = "glitch";
-      themePackages = with pkgs-unstable; [
-        (adi1090x-plymouth-themes.override {
-          selected_themes = [ "glitch" ];
-        })
-      ];
-    };
-    consoleLogLevel = 0;
-    initrd.verbose = false;
     kernelParams = [
-      "quiet"
-      "splash"
-      "boot.shell_on_fail"
-      "loglevel=3"
-      "rd.systemd.show_status=false"
-      "rd.udev.log_level=3"
-      "udev.log_priority=3"
       "nohibernate"
       "apparmor=1"
       "security=apparmor"
