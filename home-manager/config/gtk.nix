abc7b8900fe3cb30cf247b159aa468c20f1f5911
ab0z3in4 <169134542+ab0z3in4@users.noreply.github.com>
2025-04-13 11:18:23 +0200

    update

--- home-manager/config/gtk.nix
+++ home-manager/config/gtk.nix
@@ -7,7 +7,7 @@
     cursorTheme.name = "Bibata-Modern-Classic";
     cursorTheme.size = 16;
     theme.package = pkgs-unstable.gnome-themes-extra;
-    theme.name = "Adwaita";
+    theme.name = "Adwaita-dark";
     iconTheme.package = pkgs-unstable.papirus-icon-theme;
     iconTheme.name = "Papirus-Dark";
     font.name = "Sans";
@@ -47,6 +47,16 @@
       gtk-application-prefer-dark-theme = 1;
     };
     gtk4.extraConfig = {
+      gtk-toolbar-style = "GTK_TOOLBAR_ICONS";
+      gtk-toolbar-icon-size = "GTK_ICON_SIZE_LARGE_TOOLBAR";
+      gtk-button-images = 0;
+      gtk-menu-images = 0;
+      gtk-enable-event-sounds = 1;
+      gtk-enable-input-feedback-sounds = 0;
+      gtk-xft-antialias = 1;
+      gtk-xft-hinting = 1;
+      gtk-xft-hintstyle = "hintslight";
+      gtk-xft-rgba = "rgb";
       gtk-decoration-layout = "appmenu:none";
       gtk-application-prefer-dark-theme = 1;
     };
