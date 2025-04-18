b73ef09144b2667acebe26e2fd8f7a08c09b7505
Mostafa Mahmoud AboZeina <169134542+ab0z3in4@users.noreply.github.com>
2025-04-18 08:14:56 +0200

    edit gtk

--- home-manager/config/gtk.nix
+++ home-manager/config/gtk.nix
@@ -6,8 +6,8 @@
     cursorTheme.package = pkgs-unstable.bibata-cursors;
     cursorTheme.name = "Bibata-Modern-Classic";
     cursorTheme.size = 16;
-    theme.package = pkgs-unstable.gnome-themes-extra;
-    theme.name = "Adwaita-dark";
+    theme.package = pkgs-unstable.orchis-theme;
+    theme.name = "Orchis-Dark-Compact";
     iconTheme.package = pkgs-unstable.papirus-icon-theme;
     iconTheme.name = "Papirus-Dark";
     font.name = "Sans";
@@ -21,16 +21,16 @@
       "file:///home/ab0z3in4/Videos Videos"
     ];
     gtk2.extraConfig = ''
-      gtk-toolbar-style=GTK_TOOLBAR_ICONS
-      gtk-toolbar-icon-size=GTK_ICON_SIZE_LARGE_TOOLBAR
-      gtk-button-images=0
-      gtk-menu-images=0
-      gtk-enable-event-sounds=1
-      gtk-enable-input-feedback-sounds=0
-      gtk-xft-antialias=1
-      gtk-xft-hinting=1
-      gtk-xft-hintstyle="hintslight"
-      gtk-xft-rgba="rgb"
+      gtk-toolbar-style = GTK_TOOLBAR_ICONS
+      gtk-toolbar-icon-size = GTK_ICON_SIZE_LARGE_TOOLBAR
+      gtk-button-images = 0
+      gtk-menu-images = 0
+      gtk-enable-event-sounds = 1
+      gtk-enable-input-feedback-sounds = 0
+      gtk-xft-antialias = 1
+      gtk-xft-hinting = 1
+      gtk-xft-hintstyle = "hintslight"
+      gtk-xft-rgba = "rgb"
     '';
     gtk3.extraConfig = {
       gtk-toolbar-style = "GTK_TOOLBAR_ICONS";
