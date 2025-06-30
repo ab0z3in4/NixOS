8cc21351ebac1a544c2b9bc942e317562b2f8252
ab0z3in4 <169134542+ab0z3in4@users.noreply.github.com>
2025-06-30 10:24:25 +0300

    Update virtualisation.nix

--- system/modules/virtualisation.nix
+++ system/modules/virtualisation.nix
@@ -7,6 +7,7 @@
       package = pkgs-unstable.libvirt;
       qemu = {
         package = pkgs-unstable.qemu_kvm;
+        runAsRoot = false;
         swtpm = {
           enable = true;
           package = pkgs-unstable.swtpm;
